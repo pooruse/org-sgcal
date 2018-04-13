c7785fa33534868f49a8235329a6b5830c101946
Sheng Wen Peng <pooruse@gmail.com>
2018-04-13 21:04:33 +0800

    Add test for update tokens and fetch all

--- test/org-sgcal-test.el
+++ test/org-sgcal-test.el
@@ -19,11 +19,14 @@ as `decode-time' return"
        (apply #'encode-time
               date-time))
     (progn
-      (setcar (nthcdr 2 date-time) 0)
       (format-time-string
        (car org-time-stamp-formats)
        (apply #'encode-time
-              date-time)))))
+	      `(0 0 0
+		  ,(nth 3 date-time)
+		  ,(nth 4 date-time)
+		  ,(nth 5 date-time)
+		  nil))))))
 
 (ert-deftest test-org-sgcal/create-headline ()
   "test org-sgcal--create-headline can 
@@ -291,3 +294,75 @@ replace headline currectly"
                  "2018-04-01T17:00:00+0800"))
   (should (equal (org-sgcal--convert-time-string "2018-04-01T17:00:00Z")
                  "2018-04-01T17:00:00Z")))
+
+(defun dummy-request-token (client-id client-secret nickname)
+  "return dummy token for test"
+  '((access_token . (concat
+		     "aacceess"
+		     client-id
+		     client-secret
+		     nickname))
+    (refresh_token . "rreeffrr")))
+
+(defun dummy-refresh-token (client-id client-secret refresh-token)
+  "return dummy token for test (only access_token"
+  '((refresh_token . (concat"new_rreeffrr"))))
+
+(defun dummy-events-list (&rest argv)
+  "return dummy events list for test"
+  '((items . [(
+	       (id . "test")
+	       (description . "Hello word")
+	       (start . ((date . "2018-04-01")))
+	       (end . ((date . "2018-04-02")))
+	       (updated . "2018-01-01T01:02:03Z")
+	       (description . "Hello word")
+	       (summary . "hee"))
+	      (
+	       (id . "test2")
+	       (description . "Poo boo")
+	       (start . ((dateTime . "2018-04-01T08:00:00+0800")))
+	       (end . ((dateTime . "2018-04-02T20:00:00+0800")))
+	       (updated . "2018-01-01T01:02:03Z")
+	       (description . "Hello word")
+	       (summary . "hee"))]))
+  )
+(ert-deftest test-org-sgcal/token-and-fetch ()
+  "test for update-tokens-alist and update-level3-headlines"
+  (should (equal (with-temp-buffer
+		   (insert (concat "* My self\n"
+				   "  :PROPERTIES:\n"
+				   "  :CLIENT-ID: test-client-id\n"
+				   "  :CLIENT-SECRET: test-client-secret\n"
+				   "  :END:\n"
+				   "** main-cal\n"
+				   "   :PROPERTIES:\n"
+				   "   :CALENDAR-ID: test_cid\n"
+				   "   :END:\n"))
+		   (org-sgcal--update-token-alist #'dummy-request-token #'dummy-refresh-token)
+		   (org-sgcal--update-level3-headlines #'dummy-events-list)
+		   (buffer-string))
+		 (concat "* My self\n"
+			 ":PROPERTIES:\n"
+			 ":CLIENT-ID: test-client-id\n"
+			 ":CLIENT-SECRET: test-client-secret\n"
+			 ":END:\n"
+			 "** main-cal\n"
+			 "  :PROPERTIES:\n"
+			 "  :CALENDAR-ID: test_cid\n"
+			 "  :END:\n"
+			 "*** hee\n"
+			 "   DEADLINE: <2018-04-02 一> SCHEDULED: <2018-04-01 日>\n"
+			 "   :PROPERTIES:\n"
+			 "   :ID:       test\n"
+			 "   :UPDATED:  2018-01-01T01:02:03Z\n"
+			 "   :END:\n"
+			 "   Hello word\n"
+			 "*** hee\n"
+			 "   DEADLINE: <2018-04-02 一 20:00> SCHEDULED: <2018-04-01 日 08:00>\n"
+			 "   :PROPERTIES:\n"
+			 "   :ID:       test2\n"
+			 "   :UPDATED:  2018-01-01T01:02:03Z\n"
+			 "   :END:\n"
+			 "   Poo boo\n"))))
+
