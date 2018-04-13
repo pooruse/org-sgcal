fcff3e587561855ae13b4466d3997a8dc18bea2a
derek <derek.peng@ackuretta.com>
2018-04-13 16:05:21 +0800

    Concat test strings

--- test/org-sgcal-test.el
+++ test/org-sgcal-test.el
@@ -21,14 +21,13 @@ create correct org string"
 				      "Oh my god!"
 				      '(nil nil nil 4 4 2018)
 				      '(nil nil nil 3 5 2018)))
-	  "* TODO test
-DEADLINE: <2018-05-03 四> SCHEDULED: <2018-04-04 三>
-:PROPERTIES:
-:proper1:  1234
-:proper2:  abcde
-:END:
-Oh my god!
-"))
+	  (concat"* TODO test\n"
+                 "DEADLINE: <2018-05-03 四> SCHEDULED: <2018-04-04 三>\n"
+                 ":PROPERTIES:\n"
+                 ":proper1:  1234\n"
+                 ":proper2:  abcde\n"
+                 ":END:\n"
+                 "Oh my god!\n")))
   (should
      (equal (org-element-interpret-data
 	     (org-sgcal--create-headline '("test" 1 "TODO")
@@ -36,11 +35,9 @@ Oh my god!
 					"Oh my god!"
 					'(nil nil nil 4 4 2018)
 					'(nil nil nil 3 5 2018)))
-	    "* TODO test
-DEADLINE: <2018-05-03 四> SCHEDULED: <2018-04-04 三>
-Oh my god!
-"
-	    ))
+	    (concat "* TODO test\n"
+                    "DEADLINE: <2018-05-03 四> SCHEDULED: <2018-04-04 三>\n"
+                    "Oh my god!\n")))
   (should
    (equal (org-element-interpret-data
 	   (org-sgcal--create-headline '("test" 1 "TODO")
@@ -48,15 +45,13 @@ Oh my god!
 				      "Oh my god!"
 				      nil
 				      '(nil nil nil 3 5 2018)))
-	  "* TODO test
-DEADLINE: <2018-05-03 四>
-:PROPERTIES:
-:proper1:  1234
-:proper2:  abcde
-:END:
-Oh my god!
-"
-	  ))
+	  (concat "* TODO test\n"
+                  "DEADLINE: <2018-05-03 四>\n"
+                  ":PROPERTIES:\n"
+                  ":proper1:  1234\n"
+                  ":proper2:  abcde\n"
+                  ":END:\n"
+                  "Oh my god!\n")))
   (should
    (equal (org-element-interpret-data
 	   (org-sgcal--create-headline '("test" 1 "TODO")
@@ -64,13 +59,12 @@ Oh my god!
 				      "Oh my god!"
 				      nil
 				      nil))
-	  "* TODO test
-:PROPERTIES:
-:proper1:  1234
-:proper2:  abcde
-:END:
-Oh my god!
-"))
+	  (concat "* TODO test\n"
+                  ":PROPERTIES:\n"
+                  ":proper1:  1234\n"
+                  ":proper2:  abcde\n"
+                  ":END:\n"
+                  "Oh my god!\n")))
   (should
    (equal (org-element-interpret-data
 	   (org-sgcal--create-headline '("test" 1 nil)
@@ -78,14 +72,12 @@ Oh my god!
 				      "Oh my god!"
 				      nil
 				      nil))
-	  "* test
-:PROPERTIES:
-:proper1:  1234
-:proper2:  abcde
-:END:
-Oh my god!
-"
-	  ))
+	  (concat "* test\n"
+                  ":PROPERTIES:\n"
+                  ":proper1:  1234\n"
+                  ":proper2:  abcde\n"
+                  ":END:\n"
+                  "Oh my god!\n")))
   (should
    (equal (org-element-interpret-data
 	   (org-sgcal--create-headline '("test" 1 "TODO")
@@ -93,14 +85,13 @@ Oh my god!
 				      "Oh my god!"
 				      '(nil 40 3 4 4 2018)
 				      '(nil 50 4 3 5 2018)))
-	  "* TODO test
-DEADLINE: <2018-05-03 四 04:50> SCHEDULED: <2018-04-04 三 03:40>
-:PROPERTIES:
-:proper1:  1234
-:proper2:  abcde
-:END:
-Oh my god!
-")))
+	  (concat "* TODO test\n"
+                  "DEADLINE: <2018-05-03 四 04:50> SCHEDULED: <2018-04-04 三 03:40>\n"
+                  ":PROPERTIES:\n"
+                  ":proper1:  1234\n"
+                  ":proper2:  abcde\n"
+                  ":END:\n"
+                  "Oh my god!\n"))))
 
 
 (ert-deftest test-org-sgcal/replace-element ()
@@ -109,22 +100,20 @@ replace headline currectly"
   (should
    (equal
     (with-temp-buffer
-      (insert "* TODO test
-DEADLINE: <2018-05-03 四> SCHEDULED: <2018-04-04 三>
-:PROPERTIES:
-:proper1:  1234
-:proper2:  abcde
-:END:
-Oh my god!
-* DONE test3
-DEADLINE: <2018-07-03 四> SCHEDULED: <2018-04-04 三>
-:PROPERTIES:
-:proper1:  weqre
-:proper2:  bbb
-:END:
-Oh my godness!
-"
-	      )
+      (insert (concat "* TODO test\n"
+                      "DEADLINE: <2018-05-03 四> SCHEDULED: <2018-04-04 三>\n"
+                      ":PROPERTIES:\n"
+                      ":proper1:  1234\n"
+                      ":proper2:  abcde\n"
+                      ":END:\n"
+                      "Oh my god!\n"
+                      "* DONE test3\n"
+                      "DEADLINE: <2018-07-03 四> SCHEDULED: <2018-04-04 三>\n"
+                      ":PROPERTIES:\n"
+                      ":proper1:  weqre\n"
+                      ":proper2:  bbb\n"
+                      ":END:\n"
+                      "Oh my godness!\n"))
       (let ((ele (org-element-parse-buffer)))
 	(org-element-map ele
 	    'headline
@@ -134,15 +123,14 @@ Oh my godness!
 	    t) nil t) )
       (buffer-string))
 
-    "* DONE test
-* DONE test3
-DEADLINE: <2018-07-03 四> SCHEDULED: <2018-04-04 三>
-:PROPERTIES:
-:proper1:  weqre
-:proper2:  bbb
-:END:
-Oh my godness!
-")))
+    (concat "* DONE test\n"
+            "* DONE test3\n"
+            "DEADLINE: <2018-07-03 四> SCHEDULED: <2018-04-04 三>\n"
+            ":PROPERTIES:\n"
+            ":proper1:  weqre\n"
+            ":proper2:  bbb\n"
+            ":END:\n"
+            "Oh my godness!\n"))))
  
 (ert-deftest test-org-sgcal/parse-event-list ()
   
@@ -157,14 +145,13 @@ Oh my godness!
 										 (description . "Hello word")
 										 (summary . "hee"))])) 2))
 
-	   "** hee
-DEADLINE: <2018-04-02 一> SCHEDULED: <2018-04-01 日>
-:PROPERTIES:
-:ID:       test
-:UPDATED:  2018-01-01T01:02:03Z
-:END:
-Hello word
-"))
+	   (concat "** hee\n"
+                   "DEADLINE: <2018-04-02 一> SCHEDULED: <2018-04-01 日>\n"
+                   ":PROPERTIES:\n"
+                   ":ID:       test\n"
+                   ":UPDATED:  2018-01-01T01:02:03Z\n"
+                   ":END:\n"
+                   "Hello word\n")))
   (should (equal
 	   (org-element-interpret-data (org-sgcal--parse-event-list '((items . [(
 										 (id . "test")
@@ -175,87 +162,82 @@ Hello word
 										 (description . "Hello word")
 										 (summary . "hee"))])) 2))
 
-	   "** hee
-DEADLINE: <2018-04-02 一 18:20> SCHEDULED: <2018-04-01 日 14:30>
-:PROPERTIES:
-:ID:       test
-:UPDATED:  2018-01-01T01:02:03Z
-:END:
-Hello word
-")))
+	   (concat "** hee\n"
+                   "DEADLINE: <2018-04-02 一 18:20> SCHEDULED: <2018-04-01 日 14:30>\n"
+                   ":PROPERTIES:\n"
+                   ":ID:       test\n"
+                   ":UPDATED:  2018-01-01T01:02:03Z\n"
+                   ":END:\n"
+                   "Hello word\n"))))
 
 (ert-deftest test-org-sgcal/headline-map ()
   "test for `org-sgcal--headline-map'"
   (should (equal
            (with-temp-buffer
-             (insert "* RD Team
-  :PROPERTIES:
-  :CLIENT-ID: asdlfkjadkjfhasjkdhfas
-  :CLIENT-SECRET: 12341283461278561
-  :END:
-** sub title
-   :PROPERTIES:
-   :CALENDAR-ID: abcde
-   :END:
-** sub title2
-   :PROPERTIES:
-   :CALENDAR-ID: eeeee
-   :END:")
+             (insert (concat "* RD Team\n"
+                             "  :PROPERTIES:\n"
+                             "  :CLIENT-ID: asdlfkjadkjfhasjkdhfas\n"
+                             "  :CLIENT-SECRET: 12341283461278561\n"
+                             "  :END:\n"
+                             "** sub title\n"
+                             "   :PROPERTIES:\n"
+                             "   :CALENDAR-ID: abcde\n"
+                             "   :END:\n"
+                             "** sub title2\n"
+                             "   :PROPERTIES:\n"
+                             "   :CALENDAR-ID: eeeee\n"
+                             "   :END:"))
              (let ((ele (org-element-parse-buffer)))
                (org-sgcal--headline-map
                 1 ele (lambda (h1)
                         (setq h1 (org-element-put-property h1 :title "Biggg"))))
                (org-element-interpret-data ele)))
-           "* Biggg
-:PROPERTIES:
-:CLIENT-ID: asdlfkjadkjfhasjkdhfas
-:CLIENT-SECRET: 12341283461278561
-:END:
-** sub title
-:PROPERTIES:
-:CALENDAR-ID: abcde
-:END:
-** sub title2
-:PROPERTIES:
-:CALENDAR-ID: eeeee
-:END:
-"))
+           (concat "* Biggg\n"
+                   ":PROPERTIES:\n" ":CLIENT-ID: asdlfkjadkjfhasjkdhfas"
+                   ":CLIENT-SECRET: 12341283461278561\n"
+                   ":END:\n"
+                   "** sub title\n"
+                   ":PROPERTIES:\n"
+                   ":CALENDAR-ID: abcde\n"
+                   ":END:\n"
+                   "** sub title2\n"
+                   ":PROPERTIES:\n"
+                   ":CALENDAR-ID: eeeee\n"
+                   ":END:\n")))
   (should (equal
            (with-temp-buffer
-             (insert "* RD Team
-  :PROPERTIES:
-  :CLIENT-ID: asdlfkjadkjfhasjkdhfas
-  :CLIENT-SECRET: 12341283461278561
-  :END:
-** sub title
-   :PROPERTIES:
-   :CALENDAR-ID: abcde
-   :END:
-** sub title2
-   :PROPERTIES:
-   :CALENDAR-ID: eeeee
-   :END:")
+             (insert "* RD Team\n  \n"
+                     ":PROPERTIES:\n"
+                     "  :CLIENT-ID: asdlfkjadkjfhasjkdhfas\n"
+                     "  :CLIENT-SECRET: 12341283461278561\n"
+                     "  :END:\n"
+                     "** sub title\n"
+                     "   :PROPERTIES:\n"
+                     "   :CALENDAR-ID: abcde\n"
+                     "   :END:\n"
+                     "** sub title2\n"
+                     "   :PROPERTIES:\n"
+                     "   :CALENDAR-ID: eeeee\n"
+                     "   :END:")
              (let ((ele (org-element-parse-buffer)))
                (org-sgcal--headline-map
                 2 ele (lambda (h1 h2)
                         (setq h1 (org-element-put-property h1 :title "Biggg"))
                         (setq h2 (org-element-put-property h2 :title "Smalll"))))
                (org-element-interpret-data ele)))
-           "* Biggg
-:PROPERTIES:
-:CLIENT-ID: asdlfkjadkjfhasjkdhfas
-:CLIENT-SECRET: 12341283461278561
-:END:
-** Smalll
-:PROPERTIES:
-:CALENDAR-ID: abcde
-:END:
-** Smalll
-:PROPERTIES:
-:CALENDAR-ID: eeeee
-:END:
-"))
-  )
+           "* Biggg\n"
+           ":PROPERTIES:\n"
+           ":CLIENT-ID: asdlfkjadkjfhasjkdhfas\n"
+           ":CLIENT-SECRET: 12341283461278561\n"
+           ":END:\n"
+           "** Smalll\n"
+           ":PROPERTIES:\n"
+           ":CALENDAR-ID: abcde\n"
+           ":END:\n"
+           "** Smalll\n"
+           ":PROPERTIES:\n"
+           ":CALENDAR-ID: eeeee\n"
+           ":END:\n")))
 
 (ert-deftest test-org-sgcal/convert-time-string ()
   "test for org-sgcal--convert-time-string"
